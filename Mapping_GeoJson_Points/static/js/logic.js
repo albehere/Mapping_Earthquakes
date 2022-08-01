cc22260a5b3168e383c06dc255ff99111efd9c79
Al Yee <albertspyee@gmail.com>
2022-08-01 13:50:57 -0700

    step 13.5.2

--- Mapping_GeoJson_Points/static/js/logic.js
+++ Mapping_GeoJson_Points/static/js/logic.js
@@ -1,25 +1,42 @@
-// Get data from cities.js
-let cityData = cities;
+
 
 // Create the map object with a center and zoom level.
 var map = L.map("mapid", {
     center: [
-      37.6213, -122.3790
+      37.5, -122.5
     ],
-    zoom: 5
+    zoom: 10
   });
 
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.3790],
-  [40.7899, -111.9791],
-  [47.4502, -122.3088]
-];
 
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-  color: "yellow"
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+    console.log(feature);
+    return L.marker(latlng)
+    .bindPopup("<h2>" + feature.properties.city + "</h2>"); 
+   }
 }).addTo(map);
 
 // Loop through the cities array and create one marker for each city.
@@ -46,8 +63,8 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   // id: "mapbox/dark-v10",
-  //id: "mapbox/streets-v11",
-  id: "mapbox/satellite-streets-v11",
+  id: "mapbox/streets-v11",
+  //id: "mapbox/satellite-streets-v11",
   // id: "mapbox/light-v10",
   accessToken: API_KEY
 });
