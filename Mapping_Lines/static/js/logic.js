811f7fe7cdb234936936e865a1efa952c6dedaa3
Al Yee <albertspyee@gmail.com>
2022-08-01 13:11:56 -0700

    added multiple lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -4,20 +4,33 @@ let cityData = cities;
 // Create the map object with a center and zoom level.
 var map = L.map("mapid", {
     center: [
-      34.0522, -118.2437
+      37.6213, -122.3790
     ],
-    zoom: 4
+    zoom: 5
   });
 
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
+
 // Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location,{
-    radius: city.population/100000
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+// cities.forEach(function(city) {
+//   console.log(city)
+//   L.circleMarker(city.location,{
+//     radius: city.population/100000
+//   })
+//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   .addTo(map);
+// });
 
 //  Add a marker to the map for Los Angeles, California.
 // L.circleMarker([34.0522, -118.2437], {
@@ -33,7 +46,9 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   // id: "mapbox/dark-v10",
-  id: "mapbox/streets-v11",
+  //id: "mapbox/streets-v11",
+  id: "mapbox/satellite-streets-v11",
+  // id: "mapbox/light-v10",
   accessToken: API_KEY
 });
 
